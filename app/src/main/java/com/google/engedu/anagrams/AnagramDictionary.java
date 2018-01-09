15567d50d2c5dc65265b79a828f960555c7301c7
Demo User <demouser@n227-245.mtholyoke.edu>
2018-01-09 15:03:42 -0500

    updates after day 2

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -19,9 +19,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import android.util.Log;
+import java.util.Arrays;
 
 public class AnagramDictionary {
 
@@ -30,32 +33,65 @@ public class AnagramDictionary {
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
     private ArrayList<String> wordList;
+    private HashSet<String> wordSet;
+    private HashMap<String, ArrayList<String>> lettersToWord;
 
     public AnagramDictionary(Reader reader) throws IOException {
         BufferedReader in = new BufferedReader(reader);
         String line;
-        wordList = new ArrayList<String>();
+        wordList = new ArrayList<>();
+        wordSet = new HashSet<>();
+        lettersToWord = new HashMap<>();
         while((line = in.readLine()) != null) {
             String word = line.trim();
+            //add word to list
             wordList.add(word);
+            //add word to set
+            wordSet.add(word);
+            //add word to map
+            String sortedWord = sortLetters(word);
+            if(lettersToWord.containsKey(sortedWord)) {
+                //add word to list at key
+                ArrayList<String> list = lettersToWord.get(sortedWord);
+                list.add(word);
+            } else {
+                //make new list with word and add to map
+                ArrayList<String> newList = new ArrayList<>();
+                newList.add(word);
+                lettersToWord.put(sortedWord, newList);
+            }
         }
     }
 
     private String sortLetters(String word) {
-        
+        //convert to char array and sort
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        //convert back to String and return
+        String sorted = new String(chars);
+        return sorted;
     }
 
     public boolean isGoodWord(String word, String base) {
-        return true;
+
+        return (wordSet.contains(word) && !word.contains(base));
     }
 
     public List<String> getAnagrams(String targetWord) {
         ArrayList<String> result = new ArrayList<String>();
+        String sortedTarget = sortLetters(targetWord);
+        if(lettersToWord.get(sortedTarget) != null) {
+            result = lettersToWord.get(sortedTarget);
+        }
         return result;
     }
 
     public List<String> getAnagramsWithOneMoreLetter(String word) {
         ArrayList<String> result = new ArrayList<String>();
+        for(char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            String newWord = word + alphabet;
+            result.addAll(getAnagrams(newWord));
+        }
         return result;
     }
 
